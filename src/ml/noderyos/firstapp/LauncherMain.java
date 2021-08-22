8b2a791c1e5f2cb78964b26036d675f6e800103c
Noderyos <56077132+Noderyos@users.noreply.github.com>
2021-08-22 18:53:31 +0200

    Adding final functions

---
+++ src/ml/noderyos/firstapp/LauncherMain.java
@@ -0,0 +1,21 @@
+package ml.noderyos.firstapp;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class LauncherMain extends JFrame{
+
+    public static void main(String[] args) throws Exception{
+        new LauncherMain();
+    }
+
+    public LauncherMain() throws Exception{
+        setSize(720,720);
+        setUndecorated(true);
+        setBackground(new Color(0, 0, 0, 0));
+        setContentPane(new Interface());
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
